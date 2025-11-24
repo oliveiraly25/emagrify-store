1e26707a6d44a066b7bdb4bc385766075157e39a
oliveiraly25 <oliveiraly25@gmail.com>
2025-11-24 04:58:07 +0000

    add logos e ajustes no header

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Ftzl2R9nHUkBvGHCYizexGYDYgll46cVz5rc9qHItZ0=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
