5bf9d5eb813a2606c2cd81de1d0048118809af40
oliveiraly25 <oliveiraly25@gmail.com>
2025-11-27 15:05:43 -0300

    Create fonts.ts

---
+++ src/app/fonts.ts
@@ -0,0 +1,11 @@
+import localFont from "next/font/local";
+
+export const Tusca = localFont({
+  src: "../../public/fonts/Tusca.ttf",
+  variable: "--font-tusca",
+});
+
+export const Flawless = localFont({
+  src: "../../public/fonts/Flawless.ttf",
+  variable: "--font-flawless",
+});
