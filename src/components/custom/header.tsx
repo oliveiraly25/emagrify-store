a1fce02b651ab831ffbfdbbe89385c51a460d1de
oliveiraly25 <oliveiraly25@gmail.com>
2025-11-27 15:21:20 -0300

    Update header.tsx

--- src/components/custom/header.tsx
+++ src/components/custom/header.tsx
@@ -138,7 +138,7 @@ export default function Header() {
 <div className="flex-1 flex justify-center select-none">
   <h1
     className="
-      font-tusca 
+      font-flawless 
       text-3xl 
       tracking-wide 
       font-semibold 
